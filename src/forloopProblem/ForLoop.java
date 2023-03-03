6a814fb4fcad2348aaf8b30581456c1be9c6c2e1
Education-cite <tusarjobs@gmail.com>
2023-03-03 18:23:07 +0600

    first commit

---
+++ src/forloopProblem/ForLoop.java
@@ -0,0 +1,38 @@
+package forloopProblem;
+
+public class ForLoop {
+
+	public static void main(String[] args) {
+		
+		//problem 1--------print 1 to 100 which value 100,90,80,70,60,50,40,30,20,10,0--------
+		
+		int i;
+		for(i=100;i>=0;i=i-10) {
+			System.out.println(i);
+		}
+		
+		
+		// problem 2---------write a for Loop which will print summation of all the numbers 
+	//	which are divided by 3 an 5 between 30 to 100
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
